7b44bf66a5f4877e870579e2c5301ef64471d967
bnqtoan <bnqtoan@gmail.com>
2015-04-24 00:56:28 +0700

    init plugin

---
+++ wsd-essensial.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package WordpressSeniorDevelopers/essensial
+ * @version 0.1.0
+ */
+
+/*
+Plugin Name: WDS Essensial
+Plugin URI: https://github.com/WordpressSeniorDevelopers/essensial
+Description: https://github.com/WordpressSeniorDevelopers/essensial/blob/master/README.md
+Author: Wordpress Senior Developers
+Version: 0.1.0
+Author URI: https://github.com/WordpressSeniorDevelopers
+*/
